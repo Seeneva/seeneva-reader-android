7aceb4ed339c1f3faa363a41a6fede49021aca00
Sergei Solodovnikov <zellius@seeneva.app>
2024-10-09 18:44:56 +0200

    Support cutouts

--- app/src/main/kotlin/app/seeneva/reader/screen/viewer/SystemUiManager.kt
+++ app/src/main/kotlin/app/seeneva/reader/screen/viewer/SystemUiManager.kt
@@ -18,22 +18,20 @@
 
 package app.seeneva.reader.screen.viewer
 
-import android.os.Build
-import android.view.View
 import android.view.Window
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.coroutineScope
 import app.seeneva.reader.extension.insetsFlow
-import app.seeneva.reader.extension.systemUiVisibilityChange
 import app.seeneva.reader.extension.waitLayout
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 /**
@@ -78,16 +76,7 @@ interface SystemUiManager {
             lifecycle: Lifecycle,
             initState: SystemUiState = SystemUiState.HIDDEN
         ): SystemUiManager =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                SystemUiManagerInsets(window, lifecycle, initState)
-            } else {
-                @Suppress("DEPRECATION")
-                SystemUiManagerLegacy(
-                    window,
-                    lifecycle,
-                    initState
-                )
-            }
+            SystemUiManagerInsets(window, lifecycle, initState)
     }
 }
 
@@ -141,7 +130,7 @@ private abstract class BaseSystemUiManager(
 }
 
 /**
- * System UI manager for Android 11 and higher
+ * System UI manager. It uses [WindowInsetsControllerCompat]
  */
 private class SystemUiManagerInsets(
     window: Window,
@@ -152,8 +141,8 @@ private class SystemUiManagerInsets(
         get() = WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars()
 
     private val windowInsetsController by lazy {
-        checkNotNull(WindowCompat.getInsetsController(window, window.decorView)) {
-            "Window inset controller is null"
+        WindowCompat.getInsetsController(window, window.decorView).also {
+            it.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
         }
     }
 
@@ -163,9 +152,6 @@ private class SystemUiManagerInsets(
             //we need to wait till root view laid out
             //without it windowInsets could be null
             window.decorView.waitLayout()
-
-            windowInsetsController.systemBarsBehavior =
-                WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
 
             if (it.isVisible(typeMask)) {
                 SystemUiState.SHOWED
@@ -177,7 +163,7 @@ private class SystemUiManagerInsets(
 
     init {
         // This is the primary step for ensuring that your app goes edge-to-edge
-        // https://developer.android.com/training/gestures/edge-to-edge#lay-out-in-full-screen
+        // https://developer.android.com/develop/ui/views/layout/edge-to-edge-manually
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         coroutineScope.launch { stateFlow.collect(::onStateChange) }
@@ -189,122 +175,6 @@ private class SystemUiManagerInsets(
         when (state) {
             SystemUiState.SHOWED -> windowInsetsController.show(typeMask)
             SystemUiState.HIDDEN -> windowInsetsController.hide(typeMask)
-        }
-    }
-}
-
-/**
- * System UI manager for Android lower than 11
- */
-@Deprecated("Deprecated since Android 11")
-private class SystemUiManagerLegacy(
-    window: Window,
-    lifecycle: Lifecycle,
-    initState: SystemUiState
-) : BaseSystemUiManager(lifecycle) {
-    private val view = window.decorView
-
-    private val flags = Flags()
-
-    private var initStateRunnable: Runnable? = Runnable { showState(initState) }
-
-    override val stateFlow =
-        @Suppress("DEPRECATION")
-        view.systemUiVisibilityChange()
-            .map {
-                if (flags.systemUiHidden(it)) {
-                    SystemUiState.HIDDEN
-                } else {
-                    SystemUiState.SHOWED
-                }
-            }.stateIn(coroutineScope, SharingStarted.Eagerly, initState)
-
-    init {
-        @Suppress("DEPRECATION")
-        view.systemUiVisibility = flags.layout
-
-        // This should help to show proper UI state after some events like turn screen ON/OFF
-        // Or after hiding/showing the application
-        lifecycle.addObserver(object : DefaultLifecycleObserver {
-            private var stateToRestore: SystemUiState? = null
-
-            override fun onPause(owner: LifecycleOwner) {
-                super.onPause(owner)
-                stateToRestore = stateFlow.value
-            }
-
-            override fun onResume(owner: LifecycleOwner) {
-                super.onResume(owner)
-
-                // restore last state after onPause
-                stateToRestore?.also {
-                    if (stateFlow.value != it) {
-                        showState(it)
-                    }
-                }
-
-                stateToRestore = null
-            }
-        })
-
-        coroutineScope.launch { stateFlow.collect(::onStateChange) }
-
-        // If you call `showState(initState)` as is without using `post`
-        // OnSystemUiVisibilityChangeListener will not be called after screen rotation
-        // Weird...
-        view.post(initStateRunnable)
-    }
-
-    override fun showState(state: SystemUiState) {
-        if (initStateRunnable != null) {
-            view.removeCallbacks(initStateRunnable)
-            initStateRunnable = null
-        }
-
-        view.apply {
-            @Suppress("DEPRECATION")
-            systemUiVisibility = when (state) {
-                SystemUiState.SHOWED -> {
-                    systemUiVisibility and flags.hide.inv()
-                }
-
-                SystemUiState.HIDDEN -> {
-                    systemUiVisibility or flags.hide
-                }
-            }
-        }
-    }
-
-    @Suppress("DEPRECATION")
-    private interface Flags {
-        /**
-         * Hide system UI flags
-         */
-        val hide: Int
-
-        /**
-         * Layout flags
-         */
-        val layout: Int
-
-        fun systemUiHidden(flags: Int): Boolean
-
-        companion object {
-            operator fun invoke() =
-                object : Flags {
-                    override val hide = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_FULLSCREEN)
-
-                    override val layout = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-
-                    override fun systemUiHidden(flags: Int) =
-                        flags.hasFlag(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN)
-                }
-
-            private fun Int.hasFlag(flag: Int): Boolean = this and flag == flag
         }
     }
 }
